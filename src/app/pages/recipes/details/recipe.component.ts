183d92620c2e78a30f53f0e9f77c5a5d314228b6
Cdra261987 <anaritacatarino.87@gmail.com>
2024-05-13 11:09:38 +0100

    add features components

--- src/app/pages/recipes/details/recipe.component.ts
+++ src/app/pages/recipes/details/recipe.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
 import { NavBarComponent } from "../../../components/navbar/navbar.component";
+import { CommentsComponent } from "../../../components/comments/comments.component";
+import { RatingsComponent } from "../../../components/ratings/ratings.component";
 
 @Component({
     selector: 'recipe-details',
     templateUrl: './recipe.component.html',
     styleUrl: './recipe.component.css',
     standalone: true,
-    imports: [NavBarComponent]
+    imports: [NavBarComponent, CommentsComponent, RatingsComponent]
 })
 export class RecipePageDetails {}
--- src/app/pages/recipes/favorites/favorites.component.ts
+++ src/app/pages/recipes/favorites/favorites.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { NavBarComponent } from "../../../components/navbar/navbar.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: 'recipe-favorites',
     templateUrl: './favorites.component.html',
     styleUrl: './favorites.component.css',
     standalone: true,
-    imports: [NavBarComponent]
+    imports: [CommonModule, NavBarComponent]
 })
 export class RecipePageFavorites {}
